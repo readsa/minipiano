5ab7219c1df2448cd86d25964fe00d307a540f93
zhanghaiyang <834896270@qq.com>
2026-02-12 21:13:53 +0800

    Initial Commit

---
+++ minipiano/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  minipiano
+//
+//  Created by 张海洋 on 2026/2/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ minipiano/minipianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  minipianoApp.swift
+//  minipiano
+//
+//  Created by 张海洋 on 2026/2/12.
+//
+
+import SwiftUI
+
+@main
+struct minipianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
